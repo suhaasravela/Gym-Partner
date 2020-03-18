69e65c15de98114c7bfbe53de4f8d0bd619b271d
Suhaas Ravela <suhaasravela@gmail.com>
2020-03-18 18:31:51 -0400

    Initial Commit

---
+++ Seascape/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Seascape
+//
+//  Created by Suhaas Ravela on 3/18/20.
+//  Copyright © 2020 Suhaas Ravela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
